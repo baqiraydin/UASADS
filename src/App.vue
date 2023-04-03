7e888c7c701ff654f34b0064f47e76109cf54a3d
ahmad kash <aaboyazen1996@gmail.com>
2023-04-04 02:14:07 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto p-6">
+    <HeaderVue></HeaderVue>
+  </div>
+  <main>
+    <div class="container mx-auto p-6">
+      <Hero></Hero>
+      <Feature></Feature>
+      <Testimonial></Testimonial>
+    </div>
+    <Cta></Cta>
+  </main>
+  <Footer></Footer>
+</template>
+
+<script setup>
+import HeaderVue from "./sections/Header.vue";
+import Hero from "./sections/Hero.vue";
+import Feature from "./sections/Feature.vue";
+import Testimonial from "./sections/Testimonial.vue";
+import Cta from "./sections/Cta.vue";
+import Footer from "./sections/Footer.vue";
+</script>
---
+++ src/sections/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nav class="flex justify-between items-center relative">
+      <div>
+        <img src=".././assets/img/logo.svg" alt="logo" />
+      </div>
+      <NavList class="hidden md:flex space-x-4 justify-between items-center" />
+      <MainButton class="hidden self-baseline md:block bg-brightRed text-white">
+        Get Started
+      </MainButton>
+      <DropDownNav />
+    </nav>
+  </header>
+</template>
+<script setup>
+import MainButton from "../components/MainButton.vue";
+import DropDownNav from "../components/DropDownNav.vue";
+import NavList from "../components/NavList.vue";
+</script>
